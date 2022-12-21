02bc72990f9ffff36bee16840a13e1095a7c4c23
Ciberboy313 <gabrieledecarolis203@gmail.com>
2022-12-21 17:57:04 +0100

    implementazione inventario

--- prova_gioco/prova_gioco.c
+++ prova_gioco/prova_gioco.c
@@ -4,10 +4,11 @@
 
 typedef struct Personaggio
 {
-    char name[10];
-    int lvl;
-    int hp;
-    int armor;
+    char *name[10];
+    int *lvl;
+    int *hp;
+    int *armor;
+    char *inventario[10];
 } PG;
 
 void inserimento_nome(PG *pg)
@@ -16,10 +17,28 @@ void inserimento_nome(PG *pg)
     scanf("%s", pg->name);
     printf("Il tuo nome e': %s\n", pg->name);
 }
+int statistiche(PG *pg)
+{
+    pg->hp = pg->hp + 25;
+    pg->armor = pg->armor + 5;
+
+    // aumento della vita e dell'armor in base al livello
+    if (*pg->lvl > 1)
+    {
+        *pg->hp = *pg->hp + (25) * (*pg->lvl);
+        *pg->armor = *pg->armor + (5) * (*pg->lvl);
+    }
+}
+int inventario(PG *pg)
+{
+
+}
 
 int main()
 {
     PG pg;
+    *pg.lvl = 1;
     inserimento_nome(&pg);
+    statistiche(&pg);
     return 0;
 }
